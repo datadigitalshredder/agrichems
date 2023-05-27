d4c55840815a8fbe67c3dd40cb1c1757334275f8
datadigitalshredder <hov21003@byui.edu>
2023-05-27 06:34:58 +0200

    Week 6 - reg number regex

--- helpers/validate.js
+++ helpers/validate.js
@@ -9,7 +9,9 @@ const validator = (body, rules, customMessages, callback) => {
 };
 
 // Custom validation for registration numbers: uppercase letters, numbers, and hyphens only
-const registrationNumberRegex = /b[A-Z0-9]+(?:-[A-Z0-9]+)+/
+// const registrationNumberRegex = /b[A-Z0-9]+(?:-[A-Z0-9]+)+/;
+const registrationNumberRegex = /b([0-9]+-[A-Z]+)+(?:-([0-9]+-[A-Z]+))+/;
+
 // Tighten registration number policy
 Validator.register('strict', value => registrationNumberRegex.test(value),
     'Registration number must contain uppercase letters, numbers, and hyphens only');

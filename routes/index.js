2cfd4207b744f4bf3dc8f7f1b44e046b9b5f5232
datadigitalshredder <hov21003@byui.edu>
2023-06-07 16:25:41 +0200

    Week 8 commit 2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/agrichems', require('./agrichems'))
+router.use('/agrichems', require('./agrichems'));
 router.use('/user', require('./user'));
 
 module.exports = router;

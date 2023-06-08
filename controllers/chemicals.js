8b34fe3a0769ead8aea07440f7495a217038df30
datadigitalshredder <hov21003@byui.edu>
2023-06-08 15:41:54 +0200

    Week 8 GET 1

--- controllers/chemicals.js
+++ controllers/chemicals.js
@@ -33,12 +33,12 @@ const getOne = async (req, res, next) => {
     .db()
     .collection('agrichems')
     .find({ _id: userId })
-    result.toArray((err, result) => {
-      if (err) {
-        res.status(400).json({ message: err });
-      }
+    result.toArray().then((lists) => {
+      // if (err) {
+      //   res.status(400).json({ message: err });
+      // }
       res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(result[0]);
+      res.status(200).json(lists[0]);
   });
 };
 

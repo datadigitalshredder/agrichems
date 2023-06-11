60b72a3562f42ffc03070a2d72fe5177256d7827
datadigitalshredder <hov21003@byui.edu>
2023-06-11 02:43:29 +0200

    Week 8 auth sha1

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
-const express = require('express');
+// const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connection');
-const passport = require('passport')
+// const dotenv = require('dotenv')
+// const passport = require('passport')
 
+//
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo');
+const connectDB = require('./db/connection')
+//
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
 const port = process.env.PORT || 8080;
 
+// Load config
+dotenv.config({ path: './.env' })
+
 require('./db/passport')(passport)
+
+connectDB()
+
 const app = express();
 
 app

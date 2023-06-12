a0c13ca256b357e2e32a246164fff3b2072a348d
datadigitalshredder <hov21003@byui.edu>
2023-06-12 08:26:44 +0200

    Week 8 Working on Client_ID

--- db/passport.js
+++ db/passport.js
@@ -13,7 +13,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        callbackURL: 'https://pesticides.onrender.com/auth/google/callback',
       },
       async (accessToken, refreshToken, profile, done) => {
         //get the user data from google 

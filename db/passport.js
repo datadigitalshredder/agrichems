04850a587c743ed699fa7e907d1c63d415d13c93
datadigitalshredder <hov21003@byui.edu>
2023-06-14 03:20:02 +0200

    Week 8 deploy test 2

--- db/passport.js
+++ db/passport.js
@@ -2,7 +2,7 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const User = require('../controllers/User')
+const User = require('../controllers/user')
 dotenv.config({ path: '../.env' })
 // test
 
